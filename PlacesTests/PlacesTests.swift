9d71dad5e72e6cd44e49e92e02a306e664331d18
jchackDragon <juancarloslopez@juans-macbook-pro.local>
2017-01-14 04:32:52 -0400

    Initial Commit

---
+++ PlacesTests/PlacesTests.swift
@@ -0,0 +1,36 @@
+//
+//  PlacesTests.swift
+//  PlacesTests
+//
+//  Created by Juan Carlos Lopez on 1/14/17.
+//  Copyright © 2017 Juan Carlos Lopez. All rights reserved.
+//
+
+import XCTest
+@testable import Places
+
+class PlacesTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
